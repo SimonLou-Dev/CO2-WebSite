840bcf2f12cf5a67ccc9a8b89074c227b7233e20
Simon Lou <simon.lou.dev@proton.me>
2023-12-11 22:18:49 +0100

    making test OK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{str_replace('_','-', app()->getLocale())}}">
 <head>
-
-<?php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-    header('Access-Control-Allow-Headers: *');
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    }
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
-
-}
-?>
-<!-- GET CSRF TOKEN -->
+    
 
     <title>Simon Lou Template</title>
 
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->getJson('/');
 
-        $response->assertStatus(200);
+        $response->assertOk();
     }
 }
